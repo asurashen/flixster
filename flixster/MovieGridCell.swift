e19192759aa16d89decbb743bc57830b0601de00
asurashen <39765452+asurashen@users.noreply.github.com>
2022-03-07 21:18:13 -0500

    flix2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by asura shen on 3/7/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+
+    
+    
+}
